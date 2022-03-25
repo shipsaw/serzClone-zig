d88a5a06aea2c57d180fa94737a3368789b8b8ad
Paul Kirby <kirby.paulh@gmail.com>
2022-03-25 00:28:07 -0400

    Experimenting with bin file parsing

--- src/main.zig
+++ src/main.zig
@@ -6,18 +6,29 @@ pub fn main() anyerror!void {
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     defer arena.deinit();
     const allocator = arena.allocator();
-
     var file = try std.fs.cwd().openFile("testFiles/test.bin", .{});
-
     const fileResult = try file.readToEndAlloc(allocator, size_limit);
-    const result = if (verifyPrelude(fileResult[0..4]) == true) "OK" else "INVALID FILE";
+    try beginParse(fileResult);
+    const result = if (verifyPrelude(fileResult[0..]) == true) "OK" else "INVALID FILE";
     try stdout.print("File status: {s}", .{result});
 }
 
-fn verifyPrelude(preludeBytes: *[4]u8) bool {
+// Verify File begins with the prelude "SERZ"
+fn verifyPrelude(preludeBytes: []u8) bool {
     const correctPrelude = "SERZ";
-    for (preludeBytes) |char, i| {
-        if (char != correctPrelude[i]) return false;
+    for (correctPrelude) |char, i| {
+        if (char != preludeBytes[i]) return false;
     }
     return true;
+}
+
+fn beginParse(fileBytes: []const u8) !void {
+    var i: usize = 0;
+    while (i < fileBytes.len) : (i += 1) {
+        if (fileBytes[i] == 0xFF and fileBytes[i + 1] == 0xFF) {
+            var ffNum = std.mem.readIntSlice(u32, fileBytes[i + 2 ..], std.builtin.Endian.Little);
+            std.debug.print("WORD: {s}\n", .{fileBytes[i + 6 .. i + 6 + ffNum]});
+        }
+        i += 1;
+    }
 }
