690719e1604db31ba47022c3aedcf67c144db11a
Paul Kirby <kirby.paulh@gmail.com>
2022-03-27 00:55:29 -0400

    More rough POC work on parsing ff56 types

--- src/main.zig
+++ src/main.zig
@@ -28,6 +28,9 @@ fn parse(fileBytes: []const u8, tabs: u8) void {
         0x50 => {
             print50(fileBytes, tabs);
         },
+        0x56 => {
+            print56(fileBytes, tabs);
+        },
         else => std.debug.print("Not Parsable: {x}\n", .{fileBytes[1]}),
     }
 }
@@ -45,6 +48,37 @@ fn print50(fileBytes: []const u8, tabs: u8) void {
         nodeName = fileBytes[wordOffset .. wordOffset + wordLen];
         std.debug.print("{s}>\n", .{nodeName});
         parse(fileBytes[wordOffset + wordLen + 8 ..], tabs + 1);
+    }
+    i = 0;
+    while (i < tabs) : (i += 1) {
+        std.debug.print("\t", .{});
+    }
+    std.debug.print("</{s}>\n", .{nodeName});
+    return;
+}
+
+fn print56(fileBytes: []const u8, tabs: u8) void {
+    var i: u8 = 0;
+    while (i < tabs) : (i += 1) {
+        std.debug.print("\t", .{});
+    }
+    std.debug.print("<(56)", .{});
+    var nodeName: []const u8 = "";
+    if (fileBytes[3] == 0xFF) {
+        const wordOffset = 8;
+        var wordLen = std.mem.readIntSlice(u32, fileBytes[4..], std.builtin.Endian.Little);
+        nodeName = fileBytes[wordOffset .. wordOffset + wordLen];
+        var attrOffset: usize = wordOffset + wordLen;
+        var attrName: []const u8 = undefined;
+        var attrLen: usize = 2;
+        if (fileBytes[attrOffset] == 0xFF) {
+            attrLen = std.mem.readIntSlice(u32, fileBytes[attrOffset + 2 ..], std.builtin.Endian.Little);
+            std.debug.print("ATTR LENGTH: {d}", .{attrLen});
+            attrName = fileBytes[attrOffset + 6 .. attrOffset + 6 + attrLen];
+            attrLen += 4 + 2;
+        }
+        std.debug.print("{s} type={s}>\n", .{ nodeName, attrName });
+        parse(fileBytes[attrOffset + attrLen + 4 ..], tabs);
     }
     i = 0;
     while (i < tabs) : (i += 1) {
