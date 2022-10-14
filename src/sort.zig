752f39ace4e08403ef15f2bd6c3bd8b265d70d53
shipsaw <kirby.paulh@gmail.com>
2022-10-13 22:27:35 -0400

    Added text node types, begin work on conversion function

--- src/sort.zig
+++ src/sort.zig
@@ -1,6 +1,6 @@
 const std = @import("std");
 const parser = @import("binParser.zig");
-const t = @import("node.zig");
+const n = @import("node.zig");
 var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
 var allocator = arena.allocator();
 const size_limit = std.math.maxInt(u32);
@@ -11,11 +11,46 @@ const Place = struct {
     hm: []const u8,
 };
 
-const textNode = struct {
-    name: ?[]const u8,
-    nType: []const u8,
-    value: ?[]const u8,
+const textNode = union {
+    ff41Node: ff41NodeT,
+    ff4eNode: ff4eNodeT,
+    ff50Node: ff50NodeT,
+    ff56Node: ff56NodeT,
+    // ff70Node: ff70NodeT,
 };
+
+const ff41NodeT = struct {
+    name: []const u8,
+    numElements: u8,
+    dType: []const u8,
+    value: []n.dataUnion,
+};
+
+const ff4eNodeT = struct {};
+
+const ff50NodeT = struct {
+    name: []const u8,
+    id: ?u32,
+    children: []textNode,
+};
+
+const ff56NodeT = struct {
+    name: []const u8,
+    dType: []const u8,
+    value: n.dataUnion,
+};
+
+// const ff70NodeT = struct {
+//     name: []const u8,
+// };
+
+fn sort(nodes: []n.node) []textNode {
+    var textNodesList = std.Arraylist(textNode).init(allocator);
+    for (nodes) |node| {
+        try textNodesList.append(convertNode(node));
+    }
+    return textNodesList.items;
+}
 
 pub fn main() !void {
     var file = try std.fs.cwd().openFile("testFiles/Scenario.bin", .{});
@@ -35,12 +70,12 @@ pub fn main() !void {
     std.debug.print("{s}", .{string.items});
 }
 
-fn convertNode(node: t.node) textNode {
+fn convertNode(node: n.node) textNode {
     return switch (node) {
-        .ff41node => textNode{ .name = node.ff41node.name },
-        .ff4enode => textNode{ .name = null },
-        .ff50node => textNode{ .name = node.ff50node.name },
-        .ff56node => textNode{ .name = node.ff56node.name },
-        .ff70node => textNode{ .name = node.ff70node.name },
+        .ff41node => textNode{ .name = node.ff41node.name, .nType = "ff41", .value = null },
+        .ff4enode => textNode{ .name = null, .nType = "ff4e", .value = null },
+        .ff50node => textNode{ .name = node.ff50node.name, .nType = "ff50", .value = n.dataUnion{ ._bool = true } },
+        .ff56node => textNode{ .name = node.ff56node.name, .nType = "ff56", .value = null },
+        .ff70node => textNode{ .name = node.ff70node.name, .nType = "ff70", .value = null },
     };
 }
